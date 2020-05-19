a30da866808975ac28dea856ad49ac86b5d3413c
z0mbi19 <32523746+z0mbi19@users.noreply.github.com>
2020-05-19 01:54:16 -0300

    input e form

---
+++ src/components/SearchBar.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+  state = { term: "" };
+
+  onFormSubmit(e) {
+    e.preventDefault();
+    console.log(this.state.term);
+  }
+
+  render() {
+    return (
+      <div className="ui segment">
+        <form onSubmit={this.onFormSubmit} className="ui form">
+          <div className="field">
+            <label>Pesquisar imagem</label>
+            <input
+              type="text"
+              onChange={(e) => this.setState({ term: e.target.value })}
+              value={this.state.term}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
